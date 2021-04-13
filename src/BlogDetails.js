c5045ef7978a45d3d955011f92012aefbba9a555
Arthur Polon <arthurpolon@hotmail.com>
2021-04-13 09:55:05 -0400

    finished classes; project working properly.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from './useFetch'
 
 
 const BlogDetails = () => {
     const {id} = useParams()
     const {data:blog, error, isLoading} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory()
+
+    const handleClick= () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -15,7 +24,8 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
-                </article>                
+                    <button onClick={handleClick}>Delete</button>
+                </article>
             )}
         </div>
     );
